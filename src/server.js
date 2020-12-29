6c23635776ce5157b9899228146dff28c9506af8
JJ Quispe <joelquispeunju@gmail.com>
2020-12-28 23:22:16 -0300

    completed tasks:
    - modified linter
    - adding prettier
    - adding pre-commit hook

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello world!' })
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello world!" });
+});
 
 app.listen(3000, (err) => {
-  if (err) console.log(err)
-  console.log('Listening in port 3000')
-})
+  if (err) console.log(err);
+  console.log("Listening in port 3000");
+});
